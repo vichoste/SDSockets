63f491643df47cb0ba0a521712e837e666f327f5
Vicente Calderón <thunderwearsvicho37@protonmail.com>
2021-10-07 17:23:18 -0300

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+
+using EzSockets;
+
+var socket = new EzSocket(null, 5000, new EzEventsListener() {
+	OnNewConnectionHandler = (EzSocket socket) => {
+		Console.WriteLine($"I'm connected to the math server");
+	},
+	OnConnectionClosedHandler = (EzSocket socket) => {
+		Console.WriteLine($"My connection to the math server has been closed");
+	},
+	OnMessageReadHandler = (EzSocket sock, byte[] buff) => {
+		Console.WriteLine($"Math server's response is: {Encoding.Default.GetString(buff)}");
+	},
+});
+
+var loop = true;
+
+socket.StartReadingMessages();
+
+do {
+	Console.WriteLine($"0) Addition - 1) Substraction - 2) Multiplication - 3) Division - 4) Exit");
+	Console.WriteLine($"Enter an option below:");
+	var input = Console.ReadLine();
+	var inputParse = int.TryParse(input, out var inputAsInt);
+	if (!inputParse) {
+		Console.WriteLine($"(!) {input} is not an integer (!)");
+		continue;
+	}
+	if (inputAsInt is >= 0 and <= 3) {
+		Console.Write($"a: ");
+		var a = Console.ReadLine();
+		var aParse = int.TryParse(a, out var aAsInt);
+		if (!aParse) {
+			Console.WriteLine($"(!) This is not an integer (!)");
+			continue;
+		}
+		Console.Write($"b: ");
+		var b = Console.ReadLine();
+		var bParse = int.TryParse(b, out var bAsInt);
+		if (!bParse) {
+			Console.WriteLine($"(!) This is not an integer (!)");
+			continue;
+		}
+		socket.SendMessage($"{inputAsInt} {a} {b}");
+	} else if (inputAsInt is 4) {
+		loop = false;
+	} else {
+		Console.WriteLine($"(!) {input} is not a valid option (!)");
+	}
+} while (loop);
+
+socket.StopReadingMessages();
+
+socket.Close();
---
+++ Server/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+
+using EzSockets;
+
+var server = new EzSocketListener(new EzEventsListener() {
+	OnNewConnectionHandler = (EzSocket socket) => {
+		Console.WriteLine($"I have a new connection");
+		socket.StartReadingMessages();
+	},
+	OnConnectionClosedHandler = (EzSocket socket) => {
+		Console.WriteLine($"My connection has been closed");
+		socket.StopReadingMessages();
+	},
+	OnMessageSendHandler = (EzSocket socket, byte[] data) => {
+		Console.WriteLine($"I've sent data: {Encoding.Default.GetString(data)}");
+	},
+	OnExceptionHandler = (EzSocket socket, Exception ex) => {
+		Console.WriteLine("My connection has been forcefully closed");
+		return ExceptionHandlerResponse.CloseSocket;
+	},
+	OnMessageReadHandler = (EzSocket socket, byte[] data) => {
+		var receivedData = Encoding.Default.GetString(data).Split(' ');
+		var operation = receivedData[0] is "0" ? "Addition" : receivedData[0] is "1" ? "Subtraction" : receivedData[0] is "2" ? "Multiplication" : receivedData[0] is "3" ? "Division" : "None";
+		var a = receivedData[1];
+		var b = receivedData[2];
+		Console.WriteLine($"I got new data:");
+		Console.WriteLine($"Operation: {operation}");
+		Console.WriteLine($"a: {a}");
+		Console.WriteLine($"b: {b}");
+		if (operation is "Division" && b is "0") {
+			socket.SendMessage($"Division by zero");
+		} else {
+			switch (operation) {
+				case "Addition":
+					socket.SendMessage($"{(int.Parse(a) + int.Parse(b))}");
+					break;
+				case "Subtraction":
+					socket.SendMessage($"{(int.Parse(a) - int.Parse(b))}");
+					break;
+				case "Multiplication":
+					socket.SendMessage($"{(int.Parse(a) * int.Parse(b))}");
+					break;
+				case "Division":
+					socket.SendMessage($"{(int.Parse(a) / int.Parse(b))}");
+					break;
+			}
+		}
+	},
+});
+
+server.Listen(5000);
